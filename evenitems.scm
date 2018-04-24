9d63caaf2482db846a4423edcc27fff833404ed6
Dakota Crowder <dcrowder2@alaska.edu>
2018-04-23 17:07:26 -0800

    Added comments

--- evenitems.scm
+++ evenitems.scm
@@ -1,6 +1,11 @@
 #lang scheme
+; evenitems
+; Dakota Crowder
+; CSCE A331 Spring 2018
+; Assignment 7 Exercise C
 
-
+; evenitems
+; Returns a list of all the even indexed items in the list passed as the parameter
 (define (evenitems a)
   (if (null? a)
     '()
